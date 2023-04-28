b87ee7625783f6db5fee85e4bb8a87b826254516
felipekmu <132099333+felipekmu@users.noreply.github.com>
2023-04-28 16:43:29 -0300

    Site Completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+//let credito = 0;
+const [ credito, alteraCredito ] = React.useState(0)
+function adicionarCrédito(){
+alteraCredito( credito + 1000 );
+}
+function compraPassagem( valor ){
+  if (credito < valor){
+    alert("você não possui créditos para comprar esta passagem")
+    return
+  }
+alteraCredito(credito - valor)
+alert ("Passagem comprada com sucesso")
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <h1> Felipe Passagens </h1>
+  <p> Conheço os mundos pelo menor preço! </p>
+  <button> Contato </button>
+  <button onClick={ ()=>adicionarCrédito() } > Meus Crédito</button>
+  <p> Seu Crédito é de <strong> R$ {credito} </strong></p>
+<h2> Viagens </h2>
+  <p> Los Angeles - R$ 1200 <button onClick= {()=>compraPassagem(1200)}> Comprar </button></p>
+  <p> Londres - R$ 1420 <button onClick= {()=>compraPassagem(1420)}> Comprar </button></p>
     </div>
   );
 }
